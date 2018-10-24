841d038c72594eac67a0947fe82eda24f28964ef
liuxunorg <33611720@qq.com>
2018-10-24 14:50:09 +0800

    add SubmarineTFInterpreter

--- submarine/src/main/java/org/apache/zeppelin/submarine/SubmarineInterpreter.java
+++ submarine/src/main/java/org/apache/zeppelin/submarine/SubmarineInterpreter.java
@@ -14,105 +14,30 @@
 
 package org.apache.zeppelin.submarine;
 
+import org.apache.zeppelin.interpreter.KerberosInterpreter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.Properties;
 
-import org.apache.zeppelin.interpreter.Interpreter;
-import org.apache.zeppelin.interpreter.InterpreterContext;
-import org.apache.zeppelin.interpreter.InterpreterException;
-import org.apache.zeppelin.interpreter.InterpreterResult;
-import org.apache.zeppelin.interpreter.thrift.InterpreterCompletion;
-import org.apache.zeppelin.scheduler.Scheduler;
-import org.apache.zeppelin.scheduler.SchedulerFactory;
-
 /**
- * Support for HBase Shell. All the commands documented here
- * http://hbase.apache.org/book.html#shell is supported.
- *
- * Requirements:
- * HBase Shell should be installed on the same machine. To be more specific, the following dir.
- * should be available: https://github.com/apache/hbase/tree/master/hbase-shell/src/main/ruby
- * HBase Shell should be able to connect to the HBase cluster from terminal. This makes sure
- * that the client is configured properly.
- *
- * The interpreter takes 3 config parameters:
- * hbase.home: Root directory where HBase is installed. Default is /usr/lib/hbase/
- * hbase.ruby.sources: Dir where shell ruby code is installed.
- *                          Path is relative to hbase.home. Default: lib/ruby
- * zeppelin.hbase.test.mode: (Testing only) Disable checks for unit and manual tests. Default: false
+ * SubmarineInterpreter of Hadoop Submarine implementation.
+ * Support for Hadoop Submarine cli. All the commands documented here
+ * https://github.com/apache/hadoop/blob/trunk/hadoop-yarn-project/hadoop-yarn/
+ * hadoop-yarn-applications/hadoop-yarn-submarine/src/site/markdown/QuickStart.md is supported.
  */
-public class SubmarineInterpreter extends Interpreter {
-  public static final String HBASE_HOME = "hbase.home";
-  public static final String HBASE_RUBY_SRC = "hbase.ruby.sources";
-  public static final String HBASE_TEST_MODE = "zeppelin.hbase.test.mode";
+public abstract class SubmarineInterpreter extends KerberosInterpreter {
+  private Logger LOGGER = LoggerFactory.getLogger(SubmarineInterpreter.class);
 
-  private Logger LOG = LoggerFactory.getLogger(SubmarineInterpreter.class);
+  public static final String HADOOP_YARN_SUBMARINE_BIN = "hadoop.yarn.submarine.bin";
+  public static final String SUBMARINE_YARN_QUEUE = "submarine.yarn.queue";
 
   public SubmarineInterpreter(Properties property) {
     super(property);
   }
 
   @Override
-  public void open() throws InterpreterException {
-
-  }
-
-  @Override
-  public void close() {
-
-  }
-
-  @Override
-  public InterpreterResult interpret(String cmd, InterpreterContext interpreterContext) {
-    return null;
-  }
-
-  @Override
-  public void cancel(InterpreterContext context) {}
-
-  @Override
   public FormType getFormType() {
     return FormType.SIMPLE;
-  }
-
-  @Override
-  public int getProgress(InterpreterContext context) {
-    return 0;
-  }
-
-  @Override
-  public Scheduler getScheduler() {
-    return SchedulerFactory.singleton().createOrGetFIFOScheduler(
-        SubmarineInterpreter.class.getName() + this.hashCode());
-  }
-
-  @Override
-  public List<InterpreterCompletion> completion(String buf, int cursor,
-      InterpreterContext interpreterContext) {
-    return null;
-  }
-
-  private static String getSystemDefault(
-      String envName,
-      String propertyName,
-      String defaultValue) {
-
-    if (envName != null && !envName.isEmpty()) {
-      String envValue = System.getenv().get(envName);
-      if (envValue != null) {
-        return envValue;
-      }
-    }
-
-    if (propertyName != null && !propertyName.isEmpty()) {
-      String propValue = System.getProperty(propertyName);
-      if (propValue != null) {
-        return propValue;
-      }
-    }
-    return defaultValue;
   }
 }
